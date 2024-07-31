58138a615060a1606ab296d9ef6d7e7765cbe8d1
Rajesh038-git <rajeshkanojia903@gmail.com>
2024-07-31 11:29:35 +0530

    second edit in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     <>
       <div>
         <div>Rajesh here</div>
+        <div>Rajesh two is here </div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
